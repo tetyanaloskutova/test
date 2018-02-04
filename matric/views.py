5c76fc611b72d864ed6696cca707adbbd05f63d2
Tetyana Loskutova <tetyana.loskutova@gmail.com>
2018-02-04 19:59:25 +0200

    Added csrf_exempt for loading data through a script
    Added csrf_exempt for loading data through a script

--- matric/views.py
+++ matric/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import SchoolRecord
 from .forms import SchoolRecordForm
 from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def matric_list(request):
 	school_records = SchoolRecord.objects.all()
@@ -12,14 +14,17 @@ def matric_detail(request, pk):
 	school_record = get_object_or_404(SchoolRecord, pk=pk)
 	return render(request, 'matric/matric_detail.html', {'school_record': school_record})   
 	
+@csrf_exempt	
 def school_record_new(request):
 	if request.method == "POST":
 		form = SchoolRecordForm(request.POST)
 		if form.is_valid():
-			post = form.save(commit=False)
+			school_record = form.save(commit=False)
 			#post.name = 'smotn'
-			post.save()
-			return redirect('matric_detail', pk=post.pk)
+			school_record.save()
+			return redirect('matric_detail', pk=school_record.pk)
+		#else:
+		#	return HttpResponse(str(request.body) + '<h1>form not valid</h1>')
 	else:
 		form = SchoolRecordForm()
 	return render(request, 'matric/matric_edit.html', {'form': form})	
